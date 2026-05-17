e88fc5c2509110b5784657e1e51b05b93a0fdbf8
Paloma Claudino <paloma.claudino@goomer.com.br>
2026-05-17 15:47:35 -0300

    Tradução para Portugues do Brasil

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 # The Manager's Handbook
 
-To contribute, just clone this repository and run the following command to start the local dev server:
-
 ```bash
 yarn
 yarn dev
 ```
-
-
-## Merging in chapter checklist
-
-[] - Add H1
-[] - Replace notion.so links
-[] - Replace images with a publically available URL (search for us-west)
-[] - Make sure quotes are quotes (rather than Notion callouts)
-[] - Fix multi-line quotes (Notion export broken)
--- next.config.js
+++ next.config.js
@@ -1,37 +1,14 @@
 const withNextra = require('nextra')('nextra-theme-book', './theme.config.js')
-const withOffline = require('next-offline')
+
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
 
 const nextConfig = {
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /\.(png|woff2|svg)(\?.*)?$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'staticOfflineCache',
-        }
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'networkFirstCache',
-        },
-      },
-    ],
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: '/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
-    ]
+  basePath: isGithubPages ? '/the-managers-handbook' : '',
+  assetPrefix: isGithubPages ? '/the-managers-handbook/' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
   },
 }
 
-module.exports = withOffline(withNextra(nextConfig))
+module.exports = withNextra(nextConfig)
