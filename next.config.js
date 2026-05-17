3bc03f93e4093fe687715cad21e36ed3381aaed5
Paloma Claudino <paloma.claudino@goomer.com.br>
2026-05-17 17:58:24 -0300

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withNextra = require('nextra')('nextra-theme-book', './theme.config.js')
 
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
+  basePath: isGithubPages ? '/the-managers-handbook' : '',
+  assetPrefix: isGithubPages ? '/the-managers-handbook/' : '',
   images: {
     loader: 'akamai',
     path: '',
