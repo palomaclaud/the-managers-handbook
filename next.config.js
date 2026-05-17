b07e1455e32ae9ff5d6364c3b3ed0456f57d354e
Paloma Claudino <paloma.claudino@goomer.com.br>
2026-05-17 17:53:35 -0300

    updating  github workflow

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withNextra = require('nextra')('nextra-theme-book', './theme.config.js')
 
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
 
 module.exports = withNextra(nextConfig)
